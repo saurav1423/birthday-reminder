1473e70545ac8a56ae6e4a16a2edc4130b670933
saurav kumar <55906348+saurav1423@users.noreply.github.com>
2021-01-05 00:21:53 +0530

    sjj

--- nodemailer.js
+++ nodemailer.js
@@ -75,68 +75,64 @@ const { SENDGRID_API } = require('./config/keys');
 // });
 
 // router.get('/sendmail', (req, res) => {
-sgMail.setApiKey(SENDGRID_API);
-function sedEmail(toEmail, userName, bdayData) {
-	if (bdayData.length == 0) {
-		// return res.json({ error: 'error' });
-		return;
-	}
-	bdayData.map((bday) => {
-		const msg = {
-			from: 'birthdayreminderofficial@gmail.com',
-			to: toEmail,
-			replyTo: 'birthdayreminderofficial@gmail.com',
-			subject: 'Your friend have birthday tommorow!',
-			text: `Hey dear ${userName}, Your Friend ${bday.fname} have brithday tommorow! `,
-		};
-		sgMail.send(msg).then(
-			() => {},
-			(error) => {
-				console.error(error);
-
-				if (error.response) {
-					console.error(error.response.body);
-				}
-			}
-		);
-	});
-}
-User.find({}, function (err, allUsers) {
-	if (err) {
-		// req.flash(
-		// 	'error',
-		// 	'We seem to be experiencing issues. Please try again later.'
-		// );
-		console.log(err);
-		// return res.json({ error: 'error' });
-		return;
-	}
-	for (var i = 0; i < allUsers.length; i++) {
-		const friends = allUsers[i].friends;
-
-		if (friends.length == 0) {
-			continue;
+cron.schedule('30 12 * * *', () => {
+	sgMail.setApiKey(SENDGRID_API);
+	function sedEmail(toEmail, userName, bdayData) {
+		if (bdayData.length == 0) {
+			return;
 		}
-		let todayBirthday = [];
-		friends.map((friend) => {
-			let bday = friend.fdob;
-			let date = bday.split('-')[2];
-			let month = bday.split('-')[1];
+		bdayData.map((bday) => {
+			const msg = {
+				from: 'birthdayreminderofficial@gmail.com',
+				to: toEmail,
+				replyTo: 'birthdayreminderofficial@gmail.com',
+				subject: 'Your friend have birthday tommorow!',
+				text: `Hey dear ${userName}, Your Friend ${bday.fname} have brithday tommorow! `,
+			};
+			sgMail.send(msg).then(
+				() => {},
+				(error) => {
+					console.error(error);
 
-			let todayDate = new Date();
-
-			if (
-				date == todayDate.getDate() + 1 &&
-				month == todayDate.getMonth() + 1
-			) {
-				todayBirthday.push(friend);
-			}
+					if (error.response) {
+						console.error(error.response.body);
+					}
+				}
+			);
 		});
-
-		console.log(todayBirthday);
-
-		sedEmail(allUsers[i].email, allUsers[i].name, todayBirthday);
 	}
+	User.find({}, function (err, allUsers) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		for (var i = 0; i < allUsers.length; i++) {
+			const friends = allUsers[i].friends;
+
+			if (friends.length == 0) {
+				continue;
+			}
+			let todayBirthday = [];
+			friends.map((friend) => {
+				let bday = friend.fdob;
+				let date = bday.split('-')[2];
+				let month = bday.split('-')[1];
+
+				let todayDate = new Date();
+
+				if (
+					date == todayDate.getDate() + 1 &&
+					month == todayDate.getMonth() + 1
+				) {
+					todayBirthday.push(friend);
+				}
+			});
+
+			console.log(todayBirthday);
+
+			sedEmail(allUsers[i].email, allUsers[i].name, todayBirthday);
+		}
+	});
 });
 // });
 
